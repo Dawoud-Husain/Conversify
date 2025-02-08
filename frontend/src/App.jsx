5033cf58ea02aba453d7bd6aee747d88f9fe71c0
ike agbaje <ikea@ike.local>
2025-02-08 00:08:28 -0500

    working on the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
+import Profile from "./pages/profile/Profile";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
@@ -11,9 +12,10 @@ function App() {
 	return (
 		<div className='p-4 h-screen flex items-center justify-center'>
 			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/' element={authUser ? <Profile /> : <Navigate to={"/login"} />} />
 				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+				<Route path='/messages' element={authUser ? <Navigate to='/' /> : <Home />} />
 			</Routes>
 			<Toaster />
 		</div>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import MessageContainer from "../../components/messages/MessageContainer";
-import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
+			<Navbar />
 			<MessageContainer />
 		</div>
 	);
