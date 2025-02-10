15c58954c2cc53ec3e5e62752f9bba324434a736
Jake McAuley <jje.mcauley@gmail.com>
2025-02-10 17:04:10 -0500

    Added more info to authContext and linked it to profile page

--- frontend/src/components/profile/ProfileHeader.jsx
+++ frontend/src/components/profile/ProfileHeader.jsx
@@ -1,32 +1,44 @@
 import { useAuthContext } from "../../context/AuthContext";
+import { useEffect } from "react";
 
 const ProfileHeader = () => {
-    const { authUser } = useAuthContext();
-    const fullName = authUser.firstName + ' ' +  authUser.lastName;
-    const username = authUser.username;
+  const { authUser } = useAuthContext();
+  const fullName = authUser.firstName + " " + authUser.lastName;
+  const username = authUser.username;
 
-    return (
-        <div className="flex flex-col self-start pb-20">
-            <div className="relative"></div>
-            <h1 className="text-3xl font-bold text-gray-900" style={{ fontFamily: "var(--header-font)" }}>{fullName}</h1>
+  useEffect(() => {
+    console.log("authUser:", authUser);
+  }, [authUser]);
 
-            <p className="text-gray-600 text-lg">@{username}</p>
+  return (
+    <div className="flex flex-col self-start pb-20">
+      <div className="relative"></div>
+      <h1
+        className="text-3xl font-bold text-gray-900"
+        style={{ fontFamily: "var(--header-font)" }}
+      >
+        {fullName}
+      </h1>
 
-            <div className="mt-4 flex space-x-4">
-                {/* <button className="px -6 py-3 bg-yellow-200 text-gray-900 font-semibold rounded-md"> */}
-                {/* <button className="btn w-44 h-8 px-8 font-semibold rounded-full btn-no-outline btn-no-outline:hover"
-                        style={{ fontFamily: "var(--header-font)" }}>
-                    Connect
-                </button>
+      <p className="text-gray-600 text-lg">@{username}</p>
 
-                <button className="btn w-44 h-8 px-8 font-semibold rounded-full border-2 btn-outline btn-no-outline:hover"
-                        style={{ fontFamily: "var(--header-font)" }}>
-                    Message
-                </button> */}
+      {/* <div className="mt-4 flex space-x-4">
+        <button className="px -6 py-3 bg-yellow-200 text-gray-900 font-semibold rounded-md" />
+        <button
+          className="btn w-44 h-8 px-8 font-semibold rounded-full btn-no-outline btn-no-outline:hover"
+          style={{ fontFamily: "var(--header-font)" }}
+        >
+          Connect
+        </button>
 
-
-            </div>
-		</div>
-	);
+        <button
+          className="btn w-44 h-8 px-8 font-semibold rounded-full border-2 btn-outline btn-no-outline:hover"
+          style={{ fontFamily: "var(--header-font)" }}
+        >
+          Message
+        </button>
+      </div> */}
+    </div>
+  );
 };
 export default ProfileHeader;
--- frontend/src/components/profile/ProfileInfo.jsx
+++ frontend/src/components/profile/ProfileInfo.jsx
@@ -1,28 +1,37 @@
 import { useAuthContext } from "../../context/AuthContext";
-import { BiEnvelope, BiHome } from "react-icons/bi"; // Importing icons
+import { BiEnvelope, BiHome, BiPhone } from "react-icons/bi"; // Importing icons
 
 const ProfileInfo = () => {
-    const { authUser } = useAuthContext();
-    // const userEmail = authUser.email;
-    
-    return (
-        <div className="relative flex flex-col gap-2 text-gray-700">
-            {/* Address Section */}
-            {/* <div className="flex items-center gap-2">
+  const { authUser } = useAuthContext();
+  const userEmail = authUser.email;
+  const phoneNumber = authUser.phoneNumber;
+
+  return (
+    <div className="relative flex flex-col gap-2 text-gray-700">
+      {/* Address Section */}
+      {/* <div className="flex items-center gap-2">
                 <BiHome className="w-8 h-8" style={{ color: 'var(--dark-yellow)' }} />
                 <span>Georgia Bay, ON Canada, A1B 2C3</span>
             </div> */}
-            <br></br>
-            {/* Email Section */}
-            
-            {/* <div className="flex items-center gap-2">
-                <h2>{userEmail}</h2>
-                <BiEnvelope className="w-7 h-7" style={{ color: 'var(--dark-yellow)' }} />
-                <a href={'mailto:johndoe@hotmail.com'} className="hover:underline">
-                {userEmail}
-                </a>
-            </div> */}
-        </div>
-    );
+
+      {/*Phone section*/}
+      <div className="flex items-center gap-2">
+        <BiPhone className="w-8 h-8" style={{ color: "var(--dark-yellow)" }} />
+        <span>{`+1 ${phoneNumber}`}</span>
+      </div>
+      <br></br>
+
+      {/* Email Section */}
+      <div className="flex items-center gap-2">
+        <BiEnvelope
+          className="w-7 h-7"
+          style={{ color: "var(--dark-yellow)" }}
+        />
+        <a href={`mailto:${userEmail}`} className="hover:underline">
+          {userEmail}
+        </a>
+      </div>
+    </div>
+  );
 };
 export default ProfileInfo;
