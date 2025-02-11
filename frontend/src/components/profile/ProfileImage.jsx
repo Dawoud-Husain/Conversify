d61ca37dc2a2eb1e1994fe289457dc72147840dc
“bhana6491” <bhana6491@gmail.com>
2025-02-10 20:42:51 -0500

    update profile picture uploading.

--- frontend/src/components/profile/ProfileImage.jsx
+++ frontend/src/components/profile/ProfileImage.jsx
@@ -1,16 +1,16 @@
 import { useAuthContext } from "../../context/AuthContext";
-
+import useGetProfile from "../../hooks/useGetProfile";
 import { Link } from "react-router-dom";
 
 const ProfileImage = () => {
 	const { authUser } = useAuthContext();
-	const profilePic = authUser.profilePic;
+	const {profile} = useGetProfile(authUser._id)
 	
 	return (
 		<div className="relative flex">
 			<Link to="/profile">
 				<img
-					src={profilePic}
+					src={profile.profilePic}
 					alt="Tailwind CSS chat bubble component"
 					className="w-44 h-44 rounded-full object-cover"
 				/>
