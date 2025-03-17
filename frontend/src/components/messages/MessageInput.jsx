ae9060d8120a116ec264eeaecb2a46c546f63f3b
ike agbaje <ikea@mac.local>
2025-03-17 12:39:45 -0400

    added close button; mostly done - just updating the UI

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -4,11 +4,11 @@ import { BsSend } from "react-icons/bs";
 import useSendMessage from "../../hooks/useSendMessage";
 import useConversation from "../../zustand/useConversation";
 
-const MessageInput = ({ replyMsg }) => {
+const MessageInput = ({ replyMsg, resetReplyMsg }) => {
 	const [message, setMessage] = useState("");
 	const { loading, sendMessage } = useSendMessage();
-    const { selectedConversation } = useConversation();
-	const { authUser } = useAuthContext();
+    // const { selectedConversation } = useConversation();
+	// const { authUser } = useAuthContext();
 
 	// const replyFromMe = replyMsg.senderId === authUser._id;
 	// const replyFromMe = authUser._id === authUser._id;
@@ -23,11 +23,12 @@ const MessageInput = ({ replyMsg }) => {
             replyMsg: replyMsg ? replyMsg._id : null, // Include replyMsg if it exists
         };
 
-		replyMsg = replyMsg ? replyMsg : null;
+		// replyMsg = replyMsg ? replyMsg : null;
 
 		console.log(messageAndReply);
 		await sendMessage(messageAndReply);
 		setMessage("");
+		resetReplyMsg();
 	};
 
 	return (
@@ -59,44 +60,5 @@ const MessageInput = ({ replyMsg }) => {
             </div>
         </form>
     );
-
-
-	// return (
-	// 	<form className='px-4 my-3' onSubmit={handleSubmit}>
-	// 		<div className='w-full relative'>
-	// 			<input
-	// 				type='text'
-	// 				className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
-	// 				placeholder='Send a message'
-	// 				value={message}
-	// 				onChange={(e) => setMessage(e.target.value)}
-	// 			/>
-	// 			<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-	// 				{loading ? <div className='loading loading-spinner'></div> : <BsSend />}
-	// 			</button>
-	// 		</div>
-	// 	</form>
-	// );
 };
 export default MessageInput;
-
-// STARTER CODE SNIPPET
-// import { BsSend } from "react-icons/bs";
-
-// const MessageInput = () => {
-// 	return (
-// 		<form className='px-4 my-3'>
-// 			<div className='w-full'>
-// 				<input
-// 					type='text'
-// 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
-// 					placeholder='Send a message'
-// 				/>
-// 				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-// 					<BsSend />
-// 				</button>
-// 			</div>
-// 		</form>
-// 	);
-// };
-// export default MessageInput;
